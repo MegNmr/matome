c8afcce07acf8d8d6a8111844adbee2840937051
MegNmr <lewisia1105@keio.jp>
2020-01-08 12:23:57 +0900

    ガチャ演出＋シーン切り替え追加（途中）

---
+++ Assets/Tips/test1/click.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class tresure_box_open : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+      //  Debug.Log("Onakasuita!");
+        this.gameObject.transform.Rotate(0, 180, 0);
+      //  Instantiate(particle, transform.position, transform.rotation);
+
+    }
+}
+
+/*
+public class tresure_box_open : MonoBehaviour
+{
+        Animation anim;
+
+        // Use this for initialization
+        void Start()
+        {
+            anim = this.gameObject.GetComponent<Animation>();
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                anim.Play();
+
+
+
+            }
+
+
+        }
+    }
+ */
---
+++ Assets/Tips/test1/particle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour
+{
+    public GameObject particle;//Particleを宣言
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        //衝突したオブジェクトがSphereだったらParticleを発生させる
+        if (other.gameObject.name == "Sphere")
+        {
+            Instantiate(particle, transform.position, transform.rotation);
+        }
+    }
+}
