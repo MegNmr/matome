66710157edadc3dd266ae2a8a9b858233269f9e0
MegNmr <lewisia1105@keio.jp>
2020-01-13 03:39:13 +0900

    パスワード変更、メールアドレス変更、ログアウト機能実装途中

--- Assets/Tips/Acceleration/current_change.cs
+++ Assets/Tips/Acceleration/current_change.cs
@@ -8,11 +8,14 @@ using System.Linq;
 public class current_change : MonoBehaviour
 {
 
-   
+
+    [SerializeField] GameObject pop;
+    
+
     private GameObject OYA;
-    private GameObject walk;
-    private GameObject height;
-    private GameObject weight;
+    private GameObject pass = null;
+    private GameObject ad = null;
+    private GameObject logout = null;
 
     private GameObject walk_change;
     private GameObject height_change;
@@ -29,21 +32,18 @@ public class current_change : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        pop.SetActive(false);
+
         OYA = GameObject.Find("Panel");
-        walk = OYA.transform.Find("current_walk").gameObject;
-        height = OYA.transform.Find("current_height").gameObject;
-        weight = OYA.transform.Find("current_weight").gameObject;
+        pass = OYA.transform.Find("pass_change").gameObject;
+        ad = OYA.transform.Find("adress_change").gameObject;
+        logout = OYA.transform.Find("logout_button").gameObject;
 
-        walk_change = OYA.transform.Find("current_walk/walk_change").gameObject;
-        height_change = OYA.transform.Find("current_height/height_change").gameObject;
-        weight_change = OYA.transform.Find("current_weight/weight_change").gameObject;
 
-    Text walk_text = walk.GetComponent<Text>();
-        walk_text.text = "現在の目標歩数: " + walkpoint;
-        Text height_text = height.GetComponent<Text>();
-        height_text.text = "現在の身長: " + heightpoint;
-        Text weight_text = weight.GetComponent<Text>();
-        weight_text.text = "現在の体重: " + weightpoint;
+       // Text walk_text = pass.GetComponent<Text>();
+       // walk_text.text = "現在の目標歩数: " + walkpoint;
+       
 
         ATO_walk = OYA.transform.Find("walk").gameObject;
         ATO_height = OYA.transform.Find("height").gameObject;
@@ -62,78 +62,94 @@ public class current_change : MonoBehaviour
 
     public void OnRetry()
     {
+
         switch (transform.name)
         {
-            case "walk_change":
-                ATO_walk.SetActive(true);
-                walk_change.SetActive(false);
-                
+            case "pass_change":
+                pass.SetActive(false);
+                ad.SetActive(false);
+                logout.SetActive(false);
+
+
                 break;
-            case "height_change":
-                ATO_height.SetActive(true);
-                height_change.SetActive(false);
+            case "adress_change":
+                pass.SetActive(false);
+                ad.SetActive(false);
+                logout.SetActive(false);
+
                 break;
-            case "weight_change":
-                ATO_weight.SetActive(true);
-                weight_change.SetActive(false);
+            case "logout_button":
+
+                pop.SetActive(true);
+
                 break;
             default:
                 break;
         }
     }
-/*
-    public void SaveRakugaki()
+
+    public void ToLogin()
     {
-        var message = _rakugakiText.text;
-        // 何も入力されていなければ処理しない
-        if (message == "")
-            return;
-
-        // ここからデータの保存処理開始
-        // 検索にはNCMBQueryを使う
-        _query = new NCMBQuery<NCMBObject>("TestClass");
-
-        // 保存されているデータ件数を取得
-        _query.CountAsync((int count, NCMBException e) => {
-            if (e != null)
-            {
-                //件数取得失敗時の処理
-                Debug.Log("件数の取得に失敗しました");
-            }
-            else
-            {
-                //データ件数が取得できていたらサーバにデータを送る
-                SendRakugakiData(count);
-            }
-        });
-
+        Application.LoadLevel("LogIn");
     }
 
-    void SendRakugakiData(int count)
+    public void ToSetting()
     {
-        // ここで指定したクラス名(=RakugakiClass)でNCMBのデータストアに登録される
-        // データストアにそのクラスがなければNCMB側で新規作成してくれる
-        // データを送る時に、newしておかないと追加ではなく上書き保存されるので注意
-        _testClass = new NCMBObject("TestClass");
+        Application.LoadLevel("Setting");
+    }
+    /*
+        public void SaveRakugaki()
+        {
+            var message = _rakugakiText.text;
+            // 何も入力されていなければ処理しない
+            if (message == "")
+                return;
 
-        // NCMBオブジェクトに値を設定する
-        // [ ]内に設定した項目名でデータストアに登録される
-        _testClass["id"] = count + 1; // データ保存件数に+1して連番のidを作成
-        _testClass["message"] = _rakugakiText.text; // 入力されたテキストをセットで設定
+            // ここからデータの保存処理開始
+            // 検索にはNCMBQueryを使う
+            _query = new NCMBQuery<NCMBObject>("TestClass");
 
-        // データストアへデータを登録する
-        _testClass.SaveAsync((NCMBException e) => {
-            if (e != null)
-            {
-                //件数取得失敗時の処理
-                Debug.Log("データの保存に失敗しました");
-            }
-            else
-            {
-                //成功時の処理
-                Debug.Log("データの保存に成功しました");
-            }
-        });
-    }*/
+            // 保存されているデータ件数を取得
+            _query.CountAsync((int count, NCMBException e) => {
+                if (e != null)
+                {
+                    //件数取得失敗時の処理
+                    Debug.Log("件数の取得に失敗しました");
+                }
+                else
+                {
+                    //データ件数が取得できていたらサーバにデータを送る
+                    SendRakugakiData(count);
+                }
+            });
+
+        }
+
+        void SendRakugakiData(int count)
+        {
+            // ここで指定したクラス名(=RakugakiClass)でNCMBのデータストアに登録される
+            // データストアにそのクラスがなければNCMB側で新規作成してくれる
+            // データを送る時に、newしておかないと追加ではなく上書き保存されるので注意
+            _testClass = new NCMBObject("TestClass");
+
+            // NCMBオブジェクトに値を設定する
+            // [ ]内に設定した項目名でデータストアに登録される
+            _testClass["id"] = count + 1; // データ保存件数に+1して連番のidを作成
+            _testClass["message"] = _rakugakiText.text; // 入力されたテキストをセットで設定
+
+            // データストアへデータを登録する
+            _testClass.SaveAsync((NCMBException e) => {
+                if (e != null)
+                {
+                    //件数取得失敗時の処理
+                    Debug.Log("データの保存に失敗しました");
+                }
+                else
+                {
+                    //成功時の処理
+                    Debug.Log("データの保存に成功しました");
+                }
+            });
+        }*/
 
 }
