f07c4f51a893abe65a91bf5bf8f7220ca2191e4a
MegNmr <lewisia1105@keio.jp>
2020-01-14 01:55:58 +0900

    体重などの情報をサーバに保存する機能を実装

--- Assets/Tips/Acceleration/UserAuth.cs
+++ Assets/Tips/Acceleration/UserAuth.cs
@@ -1,52 +1,68 @@
-﻿using UnityEngine;
-using System.Collections;
-using NCMB;
+﻿using System.Collections;
 using System.Collections.Generic;
-
-
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using NCMB;
+using System;
 public class UserAuth : MonoBehaviour
 {
-    public int flage;
     private string currentPlayerName;
-
+    private static string selfID;
+    private static string selfPass;
     // mobile backendに接続してログイン ------------------------
-
     public void logIn(string id, string pw)
     {
-
         NCMBUser.LogInAsync(id, pw, (NCMBException e) => {
             // 接続成功したら
             if (e == null)
             {
-                flage = 4;
                 currentPlayerName = id;
+                NCMBQuery<NCMBUser> query = NCMBUser.GetQuery();
+                query.WhereEqualTo("userName", id);
+                query.FindAsync((List<NCMBUser> userList, NCMBException b) => {
+                    if (b != null)
+                    {
+                        UnityEngine.Debug.Log("失敗 : " + b.Message);
+                    }
+                    else
+                    {
+                        // Debug.Log("やったーーーー！");
+                        foreach (NCMBUser user in userList)
+                        {
+                            //Debug.Log("やったーーーー！");
+                            //UnityEngine.Debug.Log("ユーザー名: " + user.UserName);
+                            selfID = user.UserName;
+                            selfPass = pw;
+                            //Debug.Log("login " + selfID);
+                        }
+                    }
+                });
             }
         });
     }
-
     // mobile backendに接続して新規会員登録 ------------------------
-
     public void signUp(string id, string mail, string pw)
     {
-
         NCMBUser user = new NCMBUser();
         user.UserName = id;
         user.Email = mail;
         user.Password = pw;
         user.SignUpAsync((NCMBException e) => {
-
             if (e == null)
             {
                 currentPlayerName = id;
             }
+            else
+            {
+                selfID = user.ObjectId;
+                selfPass = pw;
+            }
         });
     }
-
     // mobile backendに接続してログアウト ------------------------
-
     public void logOut()
     {
-
         NCMBUser.LogOutAsync((NCMBException e) => {
             if (e == null)
             {
@@ -54,16 +70,12 @@ public class UserAuth : MonoBehaviour
             }
         });
     }
-
     // 現在のプレイヤー名を返す --------------------
     public string currentPlayer()
     {
         return currentPlayerName;
     }
-
-
     // シングルトン化する ------------------------
-
     private UserAuth instance = null;
     void Awake()
     {
@@ -71,10 +83,8 @@ public class UserAuth : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
-
             string name = gameObject.name;
             gameObject.name = name + "(Singleton)";
-
             GameObject duplicater = GameObject.Find(name);
             if (duplicater != null)
             {
@@ -89,5 +99,17 @@ public class UserAuth : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    public static string returnSelfId()
+    {
+        return selfID;
+    }
+    public static string returnSelfPass()
+    {
+        return selfPass;
+    }
+    public static void changePass(string newpass)
+    {
+        selfPass = newpass;
     }
 }
